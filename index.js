36d5f9ab9ed1bf18edf047e4f385a444a322a38b
mircomartin <64798218+mircomartin@users.noreply.github.com>
2020-08-14 15:59:45 -0300

    tu mensaje

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
+
+// crea el servidor
+const app = express();
+
+//conectar db
+conectarDB()
+
+//habilitar cors
+app.use(cors());
+
+//habilitar express jason
+app.use(express.json({extended: true}))
+
+// puerto de la app 
+const PORT = process.env.PORT || 4000;
+
+//conectar rutas
+app.use('/api/usuarios', require('./routes/usuarios'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/proyectos', require('./routes/proyectos'))
+app.use('/api/tareas', require('./routes/tareas'));
+
+//arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+})
