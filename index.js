1c56a631dd4a7210f7d8340f28b4f7f5e2a8217f
mircomartin <64798218+mircomartin@users.noreply.github.com>
2020-08-15 16:59:08 -0300

    aa

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const app = express();
 conectarDB()
 
 //habilitar cors
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
+app.options("*", cors());
 
 //habilitar express jason
 app.use(express.json({extended: true}))
@@ -24,6 +25,6 @@ app.use('/api/proyectos', require('./routes/proyectos'))
 app.use('/api/tareas', require('./routes/tareas'));
 
 //arrancar la app
-app.listen(port, '0,0,0,0', () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 })
